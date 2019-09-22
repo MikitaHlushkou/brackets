2394bb9792cdde713b9681b1f2c0c8f44dd31540
Mikita Hlushkou <mikita.hlushkou@gmail.com>
2019-09-22 23:29:14 +0300

    feat:implement main function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    if (str.length % 2 != 0) {
+        return false;
+    }
+    var SolArr = [];
+    var SkobArr = str.slice('');
+    var i = 0;
+    while (i < SkobArr.length) {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if ((SolArr[SolArr.length - 1] === bracketsConfig[j][0]) &&
+                (SkobArr[i] === bracketsConfig[j][1])) {
+                SolArr.pop();
+            } else if (SkobArr[i] === bracketsConfig[j][0]) {
+                SolArr.push(SkobArr[i]);
+            }
+        }
+        i++;
+    }
+    if (SolArr.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
